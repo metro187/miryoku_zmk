ba1ef91d18a79c9f06bdda9865fa4edec0f42206
metro187 <31764720+metro187@users.noreply.github.com>
2026-02-10 01:23:50 +0100

    Update miryoku_layout.h
    updated mapping, removed RC(r,c) usage

--- config/boards/shields/vbone_pico_shield/miryoku_layout.h
+++ config/boards/shields/vbone_pico_shield/miryoku_layout.h
@@ -1,15 +1,25 @@
 // Copyright (c) 2022 Manna Harbour
 // SPDX-License-Identifier: MIT
 
-#define MIRYOKU_LAYOUT_MAPPING( \
-    K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-    K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-    K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-    N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
-) \
-RC(0,0) RC(0,1) RC(0,2) RC(0,3) RC(0,4)    RC(0,5) RC(0,6) RC(0,7) RC(0,8) RC(0,9) \
-RC(1,0) RC(1,1) RC(1,2) RC(1,3) RC(1,4)    RC(1,5) RC(1,6) RC(1,7) RC(1,8) RC(1,9) \
-RC(2,0) RC(2,1) RC(2,2) RC(2,3) RC(2,4)    RC(2,5) RC(2,6) RC(2,7) RC(2,8) RC(2,9) \
-                RC(3,2) RC(3,3) RC(3,4)    RC(3,5) RC(3,6) RC(3,7)
+// Copyright 2023 Manna Harbour
+// https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUT_MAPPING
+#if !defined (MIRYOKU_LAYOUTMAPPING_VBONE)
+
+  #define XXX &none
+
+  #define MIRYOKU_LAYOUTMAPPING_VBONE( \
+  K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+  K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+  K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+  N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+  ) \
+  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 \
+  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
+  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
+  XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
+
+  #endif
+
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_VBONE
+
